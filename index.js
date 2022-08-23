1cec1bc30107a4bdea12bcf110623f5d18ef0a4a
AnisTohme-961 <anistohme1@hotmail.com>
2022-08-23 19:45:28 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import DBCONNECT from './config/dbconnect.js';
+import StudentRoutes from './routes/student.routes.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+// INIT APP
+const app = express();
+
+app.use(express.json());
+
+app.use("/student", StudentRoutes);
+
+// APP LISTENTING
+app.listen(PORT, async () => {
+    await DBCONNECT;
+    console.log(`Server is running on port ${PORT}`);
+})
