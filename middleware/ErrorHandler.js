75a95ce2dd44bf5e29da09b95808bc75c0164fcf
AnisTohme-961 <anistohme1@hotmail.com>
2022-08-25 20:48:49 +0300

    ErrorHandler Middleware

---
+++ middleware/ErrorHandler.js
@@ -0,0 +1,9 @@
+const ErrorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Error Detected";
+    res.status(statusCode).json({
+        success: false,
+        message: message,
+        stack: process.env.NODE_ENV !== "development" ? {} : err.stack
+    })
+}
