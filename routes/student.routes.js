dda5305d95f209e423793fb106c8fcb5d3853538
AnisTohme-961 <anistohme1@hotmail.com>
2022-08-27 20:36:43 +0300

    Get and Put Routes

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,12 +1,18 @@
 import express from 'express' ;
 
-import { getStudents, addStudent } from '../controllers/student.controllers.js';
+import { getStudents, addStudent, getStudentById, getStudentByEmail, updateStudent } from '../controllers/student.controllers.js';
 
 const router = express.Router();
 
 router.get('/', getStudents);
 
 router.post('/', addStudent);
+
+router.get('/id/:id', getStudentById);
+
+router.get('/email/:email', getStudentByEmail);
+
+router.put('/id/:id', updateStudent)
 
 export default router;
 
