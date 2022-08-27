08cf5befdc80d23b610867201212617af847e824
AnisTohme-961 <anistohme1@hotmail.com>
2022-08-27 20:36:15 +0300

    Get and Update Controllers

--- controllers/student.controllers.js
+++ controllers/student.controllers.js
@@ -1,29 +1,32 @@
 import Student from '../models/student.js';
 import bcrypt from 'bcryptjs';
+import CreateError from '../util/CreateError.js';
 
-export const getStudents = ( async (req, res, next) => {
+export const getStudents = async (req, res, next) => {
 
     try {
         const students = await Student.find({});
-        const totalStudents = await Student.find({}).countDocuments();
+        if (students.length <= 0) {     // Check if students are found
+            return next(CreateError("No students found", 404))
+        }
         res.status(200).json({
             success: true,
             message: " Here we get all the students",
             students: students,
-            totalStudents: totalStudents
+            totalStudents: students.length
     })
   }
     catch (error) {
         next(error)
     }
-})
+}
 
-export const addStudent = (async (req, res, next) => {
+export const addStudent = async (req, res, next) => {
 
     const { id, firstName, lastName, email, age, pin } = req.body;
     const hashedPin = await bcrypt.hash(pin, 12);
     try {
-    const student = await new Student({
+    const student = new Student({
         id: id,
         firstName: firstName,
         lastName: lastName,
@@ -31,7 +34,7 @@ export const addStudent = (async (req, res, next) => {
         age: age,
         pin: hashedPin  
     })
-    student.save();
+    await student.save();
     res.status(201).json({
         success: true, 
         message: "Student created successfully",
@@ -41,4 +44,58 @@ export const addStudent = (async (req, res, next) => {
     catch (error) {
         next(error)
     }
-})
+}
+
+export const getStudentById = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const student = await Student.findOne({id});
+        if (!student) {
+            return next(CreateError(`Student Not Found with id ${id}`, 404))
+        }
+        res.status(200).json({
+            success: true,
+            student: student
+        })
+
+    }
+    catch (error) {
+        next(error)
+    }
+}
+
+export const getStudentByEmail = async (req, res, next) => {
+    const email = req.params.email;
+    try {
+        const student = await Student.findOne({email});
+        if (!student){
+            return next(CreateError(`Email not found of email ${email}`, 404))
+        }
+        res.status(200).json({
+            success: true,
+            student: student
+        })
+    }
+    catch(error) {
+        next(error)
+    }
+}
+
+export const updateStudent = async (req, res, next) => {
+    const id = req.params.id;
+    const {firstName, lastName, email, age} = req.body;
+    try {
+        const student = await Student.findOneAndUpdate({id}, {firstName, lastName, email, age}, {new:true});
+        if (!student) {
+            return next(CreateError(`Student not found with id ${id}`))
+        }
+        res.status(200).json({
+            success: true,
+            message: "Student updated successfully",
+            student: student
+        })
+    }
+    catch (error) {
+        next (error)
+    }
+}
