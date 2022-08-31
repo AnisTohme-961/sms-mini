cc7dbde152235d036e1b503294f1bdcc3e1e9842
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-01 02:23:42 +0300

    Staff Model

---
+++ models/staff.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const staffSchema = mongoose.Schema ({
+    name: {
+        type: String, 
+        required: [true, 'Staff name is required']
+    },
+    username: {
+        type: String,
+        required: [true, 'Staff username is required'],
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: [true, 'Staff email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Staff password is required'],
+    },
+    role: {
+        type: String,
+        enum: ["admin", "staff", "teacher"],
+        default: "staff" 
+    },
+    token: {
+        type: String,
+        default: null
+    }
+}, {timestamps: true})
+
+export default mongoose.Model('Staff', staffSchema)
